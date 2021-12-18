2292295d917fb57d4c41a0deef9b18bdc966773c
serhey <87901529+BoroRU@users.noreply.github.com>
2021-12-18 12:42:00 +0200

    add new

---
+++ js/main.js
@@ -0,0 +1,121 @@
+
+//SLICK SLAIDER
+$('.slaider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    autoplay: 3000,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+        dots:false,
+    }}
+
+    ]
+});
+
+$('.responsive').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dotsClass:'slick-btn',
+    prevArrow:$('.prev'),
+    nextArrow: $('.next'),
+    // autoplay: 1000,
+    responsive: [
+        {
+            breakpoint: 1820,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll:4,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+$('.prev').on('click', function() {
+    $('.responsive').slick('slickPrev');
+});
+$('.next').on('click', function() {
+    $('.responsive').slick('slickNext');
+});
+
+//StarTimer
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + 'мин'+ seconds + 'сек';
+
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    let sixteenMinutes = 60 * 16-30,
+        display = document.querySelector('.timer__time');
+    startTimer(sixteenMinutes, display);
+};
+
+//scroll
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector('.header').scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
+
+// //click
+// $('.battles__btn').on('click', function (){
+//     $('.responsive').toggle('.changes')
+// })
+
+
+
+
+
+
+
